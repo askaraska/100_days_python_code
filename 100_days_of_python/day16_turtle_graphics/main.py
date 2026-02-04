58f69a0d239f1011171e9b4eb67c3e8328f2ef93
SULTHAN ASKAR <sulthanaskar5@gmail.com>
2026-02-04 10:33:23 +0530

    Day 16 - turtle graphics

---
+++ 100_days_of_python/day16_turtle_graphics/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+pikky = Turtle()
+print(pikky)
+
+# shape of pikky
+pikky.shape("turtle")
+pikky.color("red")
+pikky.forward(100)
+pikky.forward(100)
+
+
+#for screen
+my_screen = Screen()
+#turtle graphics screen exit when on click
+my_screen.exitonclick()
+
